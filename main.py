c613767171c1d497b789403b825ab74608d9cad3
NormenMueller17 <normen.mueller@mailbox.org>
2025-08-23 16:40:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+from .config import SETTINGS
+from .data_sources import get_universe, load_weekly_history, load_index_series
+from .breadth import compute_breadth
+from .report_builder import build_html_report
+from .emailer import send_email
+
 def run():
   # 1) Daten laden
   universe = get_universe()
