7c08c526b40916223cea15d936365dac97ee74d3
NormenMueller17 <normen.mueller@mailbox.org>
2025-09-11 22:37:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pandas as pd
 from datetime import datetime
 from config import SETTINGS
 from data_sources import get_universe, load_weekly_history, load_index_series
-#from breadth import compute_breadth
 from breadth import compute_breadth, compute_breadth_snapshots_with_advancers as compute_breadth_snapshots
 from report_builder import build_html_report
 from emailer import send_email
-from indicators import compute_index_indicators  # falls du es nutzt
-from risk import compute_risk_metrics  # falls ausgelagert
+#from indicators import compute_index_indicators  # falls du es nutzt
+#from risk import compute_risk_metrics  # falls ausgelagert
+from screener import screen_universe_minervini
 
 from report_builder import (
     build_html_report,
@@ -26,7 +26,6 @@ def run():
 
     # 2) Kennzahlen berechnen
     breadth_df = compute_breadth(weekly)
-    #print("Breadth DataFrame:", breadth_df)
     idx_rows = build_index_rows(idx_data)
     risk_rows = build_risk_rows(idx_data)
 
@@ -34,7 +33,6 @@ def run():
     summary = heuristic_verdict(breadth_df, idx_rows)
     report_date = pd.Timestamp.now().strftime("%Y-%m-%d")
 
-    #idx_df = pd.DataFrame.from_dict(dict(idx_rows), orient="index")
     idx_df = pd.DataFrame.from_dict(dict(idx_rows), orient="index").T
     risk_df = pd.DataFrame(risk_rows, columns=["Metrik", "Aktuell", "Vorwoche", "Δ"]).set_index("Metrik")
     risk_df.rename(index={"TNX": "10Y Interest Rate (TNX)"}, inplace=True)
@@ -60,13 +58,12 @@ def run():
         for metrik, delta in zip(risk_df.index, risk_df["Δ"])
                         ]
     
-
-    #html = build_html_report(breadth_df.iloc[0], idx_df, risk_rows, summary, report_date, weekly)
-    #html = build_html_report(breadth_df, idx_df, risk_rows, summary, report_date, weekly)
+    # 4) Marktführer nach Minervini screenen
+    leaders = screen_universe_minervini(min_score=5)
+    
     html = build_html_report(breadth_df, idx_df, risk_df, summary, report_date, weekly)
-    #html = build_html_report(breadth_df, idx_df, risk_df, summary, report_date, breadth_snap)
-
-    # 4) Report senden
+    
+    # 5) Report senden
     send_email(html)
 
 
