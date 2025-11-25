edbc211351a43dcd60b97e8fd7e5211b92356e5e
NormenMueller17 <normen.mueller@mailbox.org>
2025-11-25 23:38:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def run():
     ]
     
     # 4) Marktführer nach Minervini screenen
-    leaders = screen_universe_minervini(universe, min_score=6)
+    leaders = screen_universe_minervini(universe, min_score=7)
     info_map = get_company_info_map_from_csv()
     
     if not leaders.empty:
