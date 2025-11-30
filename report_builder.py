b864ae8fbd12c7c07578bb3c3a0588c90b1e3529
NormenMueller17 <normen.mueller@mailbox.org>
2025-11-30 14:16:00 +0100

    Update report_builder.py

--- report_builder.py
+++ report_builder.py
@@ -133,12 +133,13 @@ HTML_TMPL = """
         <td class="left">{{ row["Industry"] }}</td>
         <td>{{ row["score"] }}</td>
         
-        <!--In Mail nicht alle Metriken anzeigen
+        <!--
         {% for col in leaders.columns if col not in ["score", "Company", "Industry"] %}
           {% set val = row[col] %}
           <td class="{{ 'pos' if val is sameas(true) else 'neg' if val is sameas(false) else '' }}">
             {{ "✔" if val is sameas(true) else "✘" if val is sameas(false) else (val|string) }} 
-          </td> -->
+          </td>
+           -->
         {% endfor %}
       </tr>
       {% endfor %}
